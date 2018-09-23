968279cfda86fcd23c8e321ffba7f0deb59acabb
c3budiman <c3budiman@gmail.com>
2018-09-23 14:45:33 +0700

    add filtering and loading

--- src/App.js
+++ src/App.js
@@ -1,17 +1,74 @@
 import React, { Component } from 'react';
 import './App.css';
 import BlogList from "./Components/Bloglist";
+import axios from 'axios';
+import SearchBox from "./Components/SearchBox";
+const articles = require("./Data/datac3budiman.json");
+const link = "https://c3budiman.web.id/blog/wp-json/wp/v2/posts";
 
-const link = "./Data/react-blog-example.json";
+
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      articles: [],
+      searchKey : '',
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    //console.log(axios);
+    // fetch(link)
+    // .then((data) => data.json())
+    // .then((data) => {
+    //   this.setState({
+    //     articles: data
+    //   })
+    // })
+    axios.get(link)
+    .then((res)=> {
+      console.log(res.data)
+      this.setState({
+          articles: res.data,
+          loading: false
+        })
+    })
+    .catch((err) => {
+      this.setState({
+          loading: false
+        })
+    })
+  }
+
+  doSearch = (event) => {
+    //console.log(event.target.value)
+    this.setState(
+      {searchKey : event.target.value}
+    )
+
+  }
+
   render() {
+    const loading = <h1> Memuat.... </h1>
+    const filteredArticles = this.state.articles.filter(article => (
+      article.title.rendered.toLowerCase().includes(this.state.searchKey.toLowerCase())
+    ))
+
+    const articles = filteredArticles.map((article) =>
+    <BlogList
+      article={article}
+      key={article.id}
+    />
+    )
+
     return (
       <div className="App">
-        <BlogList />
-        <BlogList />
-        <BlogList />
-        <BlogList />
+        <SearchBox onSearch={this.doSearch} />
+        <p> {this.state.searchKey} </p>
+        {this.state.loading ? loading : articles}
       </div>
     );
   }
--- src/Components/Bloglist.js
+++ src/Components/Bloglist.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 const BlogList = props => {
+  function createMarkup() {
+    return {__html: props.article.excerpt.rendered};
+  }
   return (
-    <div>
-      <h2>Tes</h2>
-      <p>lorem ipsum</p>
+    <div key={props.article.id}>
+      <h2>{props.article.title.rendered}</h2>
+      <div dangerouslySetInnerHTML={createMarkup()} />
     </div>
   );
 };
