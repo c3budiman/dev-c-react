4bb3541389370d74012ec46da61044958336d9e3
c3budiman <c3budiman@gmail.com>
2018-09-23 15:12:26 +0700

    fixing container

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
     )
 
     return (
-      <div>
+      <div className='container'>
         <SearchBox onSearch={this.doSearch} />
         {this.state.loading ? loading : articles}
       </div>
